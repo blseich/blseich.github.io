a198f286e2811ed6f7a3b03c8744adbab62567ec
blseich <blseich@gmail.com>
2014-11-17 23:42:53 -0500

    added folder specific builds for js

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,40 @@ var gulp = require('gulp');
 var autoprefix = require('gulp-autoprefixer'),
 	changed = require('gulp-changed'),
 	concat = require('gulp-concat'),
+	eventStream = require('event-stream'),
+	fs = require('fs'),
 	imagemin = require('gulp-imagemin'),
 	jshint = require('gulp-jshint'),
 	minifyCSS = require('gulp-minify-css'),
 	minifyHTML = require('gulp-minify-html'),
+	path = require('path'),
+	rename = require('gulp-rename'),
 	uglify = require('gulp-uglify'),
 	webserver = require('gulp-webserver');
 
+var srcPath = './src/',
+	dstPath = './build/';
+
+//Function for concating js and css in specific folders
+//This aides in maintaining cleaner app environments and 
+//easily removable applications
+function getFolders(dir) {
+	return fs.readdirSync(dir)
+		.filter(function(file){
+			return fs.statSync(path.join(dir, file)).isDirectory();
+		});
+}
+
+
+function buildFolderContent(func){
+	var folders = getFolders(srcPath);
+	var tasks = folders.map(func);
+	return eventStream.concat.apply(null, tasks);
+}
 
 //JS hint task
 gulp.task('jshint', function(){
-	gulp.src('./src/scripts/*.js')
+	gulp.src('./src/**/scripts/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
@@ -42,13 +65,20 @@ gulp.task('minifyHTML', function(){
 		.pipe(gulp.dest(htmlDst));
 });
 
+
 // JS concat, strip debugging and minify
-gulp.task('scripts', function() {
-  gulp.src(['./src/scripts/*.js'])
-    .pipe(concat('scripts.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/scripts/'));
+gulp.task('scripts', ['jshint'], function() {
+	var jsBuild = function(folder){
+		return gulp.src(path.join(srcPath, folder, 'scripts/*.js'))
+			.pipe(concat(folder + '.js'))
+        	.pipe(gulp.dest(dstPath + folder + '/scripts'))
+        	.pipe(uglify())
+        	.pipe(rename(folder + '.min.js'))
+        	.pipe(gulp.dest(dstPath + folder + '/scripts'));
+	}
+	return buildFolderContent(jsBuild);
 });
+
 
 // CSS concat, auto-prefix and minify
 gulp.task('styles', function() {
---
+++ src/test2/scripts/test2.js
@@ -0,0 +1,3 @@
+(function(){
+	console.log("THIS IS TEST2");
+})();
